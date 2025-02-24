493b9d6b0800900cf417913fe7cff72330980738
Jardeylson Jacinto <jardeylsong.m@gmail.com>
2025-02-24 11:45:24 -0300

    feat: created stundent model

---
+++ src/test/java/com/acc/aluno/AlunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acc.aluno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
