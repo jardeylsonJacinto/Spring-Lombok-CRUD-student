e9f1f97a5c828599f34ba913d346dc18bce1b2be
Jardeylson Jacinto <jardeylsong.m@gmail.com>
2025-02-24 16:44:00 -0300

    refactor: refactoring some archives

--- src/main/java/com/acc/aluno/AlunoApplication.java
+++ src/main/java/com/acc/aluno/AlunoApplication.java
@@ -3,11 +3,15 @@ package com.acc.aluno;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class AlunoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AlunoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        SpringApplication.run(AlunoApplication.class, args);
+    }
 }
